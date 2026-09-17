745b4eb648d756846b848b78a95246d4f522e2fc
Danielle Luz <daniln2209@gmail.com>
2026-09-17 02:12:42 -0300

    fix: opening double click behavior

--- scripts/header.js
+++ scripts/header.js
@@ -1,11 +1,14 @@
 const headerButton = document.querySelector(".header__menu-button");
+const closeIcon = document.querySelector(".header__close-icon--closed");
 
 function showCloseIcon () {
   if(headerButton.classList.contains("header__menu-button--open")) {
-    headerButton.classList.toggle("header__menu-button--closed");
+    headerButton.classList.remove("header__menu-button--open");
+    headerButton.classList.add("header__menu-button--closed");
+  } else {
+    headerButton.classList.add("header__menu-button--open");
+    headerButton.classList.remove("header__menu-button--closed");
   }
-  
-  headerButton.classList.toggle("header__menu-button--open");
 }
 
 headerButton.addEventListener("click", showCloseIcon );
